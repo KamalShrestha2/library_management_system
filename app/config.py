d257b07a8b3078de7f3665890233ca6129026c04
kamal Shrestha <whitecolour258@gmail.com>
2025-02-22 23:36:09 +0545

    Final

---
+++ app/config.py
@@ -0,0 +1,30 @@
+import os
+
+class Config:
+    # General Configuration
+    SECRET_KEY = os.environ.get('LIBRARY_SECRET_KEY')  # Default for development
+    SESSION_COOKIE_NAME = 'library_session'
+    UPLOAD_FOLDER = os.path.join('app','static', 'images', 'books')
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+    # Database Configuration
+    DATABASE_URI = os.environ.get('DATABASE_URI', 'postgresql://username:password@localhost/library_management_system')
+
+    # Debug and Testing (Explicit boolean handling)
+    DEBUG = os.environ.get('FLASK_DEBUG', 'True').lower() in ['true', '1', 'yes']
+    TESTING = os.environ.get('FLASK_TESTING', 'False').lower() in ['true', '1', 'yes']
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    TESTING = False
+
+class ProductionConfig(Config):
+    DEBUG = False
+    TESTING = False
+    SESSION_COOKIE_SECURE = True  # Ensure cookies are transmitted over HTTPS
+
+class TestingConfig(Config):
+    TESTING = True
+    DEBUG = True
+    DATABASE_URI = 'postgresql://postgres@localhost/library_management_system'  # Use a separate test DB
